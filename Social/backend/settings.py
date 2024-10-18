6be1c8aa5b8d42debe5f448ae6fd50f8bb5bc5b0
alexamew <morarualexandra597@yahoo.com>
2024-10-18 12:15:25 +0100

    Add staticfiles directory to the repository

--- Social/backend/settings.py
+++ Social/backend/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-STATIC_URL = os.path.join(BASE_DIR,'static/')
+STATIC_URL = '/static/'  # URL prefix for static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Change 'staticfiles' to your preferred directory name if needed
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
