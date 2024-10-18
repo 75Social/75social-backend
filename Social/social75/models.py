5ed45420044af65cd7fbee57a78cd7da9ff02f78
alexamew <morarualexandra597@yahoo.com>
2024-10-18 12:29:38 +0100

    add habit table

--- Social/social75/models.py
+++ Social/social75/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = HTMLField()
-    date = models.DateField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    featured = models.BooleanField(default=False)
-    likes = models.ManyToManyField(User, related_name='likes')
+class Habit(models.Model):
+    name = models.CharField(max_length=100)  # Name of the habit
+    description = models.TextField(blank=True, null=True)  # Description of the habit
+    start_date = models.DateField()  # The date the user starts the habit
+    end_date = models.DateField(blank=True, null=True)  # The date the user wants to stop the habit (optional)
+    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp when the habit was created
+    updated_at = models.DateTimeField(auto_now=True)  # Timestamp when the habit was last updated
+
 
     def get_absolute_url(self):
         return '/'
