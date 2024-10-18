f801b6ccf48da489b1b18fac93450c4237422af9
alexamew <morarualexandra597@yahoo.com>
2024-10-18 12:32:58 +0100

    refactor model

--- Social/social75/models.py
+++ Social/social75/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tinymce.models import HTMLField
 
 class Habit(models.Model):
     name = models.CharField(max_length=100)  # Name of the habit
