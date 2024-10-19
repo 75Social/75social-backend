5fe62147bb927ecc9f6b596e3e58195dce13407f
alexamew <morarualexandra597@yahoo.com>
2024-10-19 20:07:44 +0100

    add api

--- Social/backend/urls.py
+++ Social/backend/urls.py
@@ -6,5 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('social75.urls')), 
-    path('post/', views.postData),  # Includes the helloworld app's URLs
+    path('post/', views.index),  # Includes the helloworld app's URLs
 ]
